1bafe5f82c2144940b911fd1749561e2d5df8504
kamahack <kamila.felger@gmail.com>
2023-03-12 17:28:57 +0100

    additional tests for getStatement method fixed

--- BankAccount.js
+++ BankAccount.js
@@ -17,7 +17,19 @@ class BankAccount {
     }
 
     getStatement() {
-        
+        let balance = 0;
+        let statement = "date\tcredit\tdebit\tbalance\n";
+        for (let i = this.transactions.length - 1; i >= 0; i--) {
+          const transaction = this.transactions[i];
+          if (transaction.amount > 0) {
+            balance += transaction.amount;
+            statement += `${transaction.date.toLocaleDateString()}\t${transaction.amount.toFixed(2)}\t\t\t${balance.toFixed(2)}\n`;
+          } else {
+            balance += transaction.amount;
+            statement += `${transaction.date.toLocaleDateString()}\t\t\t${Math.abs(transaction.amount).toFixed(2)}\t${balance.toFixed(2)}\n`;
+          }
+        }
+        return statement;
     }
 };
 
--- BankAccount.test.js
+++ BankAccount.test.js
@@ -1,34 +1,38 @@
-const BankAccount = require('./BankAccount')
+const BankAccount = require('./BankAccount');
 
 describe('BankAccount', () => {
-    let bankAccount;
+  let bankAccount;
 
-    beforeEach(() => {
-        bankAccount = new BankAccount;
-    });
+  beforeEach(() => {
+    bankAccount = new BankAccount();
+  });
 
-    it('should start with an empty bank account', () => {
-        expect(bankAccount.balance).toEqual(0);
-    });
+  it('should start with an empty bank account', () => {
+    expect(bankAccount.balance).toEqual(0);
+  });
 
-    it('should update the bank account balance after a deposit', () => {
-      bankAccount.deposit(1000, '10/01/2023');
-      expect(bankAccount.balance).toEqual(1000);
-    });
+  it('should update the bank account balance after a deposit', () => {
+    bankAccount.deposit(1000, '10/01/2023');
+    expect(bankAccount.balance).toEqual(1000);
+  });
 
-    it('should update the bank account balance after a withdrawal', () => {
-        bankAccount.deposit(1000, '10/01/2023');
-        bankAccount.withdraw(500, '14/01/2023');
-        expect(bankAccount.balance).toEqual(500);
-    })
+  it('should update the bank account balance after a withdrawal', () => {
+    bankAccount.deposit(1000, '10/01/2023');
+    bankAccount.withdraw(500, '14/01/2023');
+    expect(bankAccount.balance).toEqual(500);
+  });
 
-    it('should generate a statement with deposits, withdrawals, and balances', () => {
-        bankAccount.deposit(1000, '10/01/2023');
-        bankAccount.deposit(2000, '13/01/2023');
-        bankAccount.withdraw(500, '14/01/2023');
-        const statement = bankAccount.getStatement();
-        expect(statement).toBe('date || credit || debit || balance\n14/01/2023 ||  || 500.00 || 2500.00\n13/01/2023 || 2000.00 ||  || 3000.00\n10/01/2023 || 1000.00 ||  || 1000.00\n');
-    })
+  it('should return an empty statement for a new account', () => {
+    expect(bankAccount.getStatement()).toBe("date\tcredit\tdebit\tbalance\n");
+  });
 
+  it('should return a statement with a single deposit transaction', () => {
+    bankAccount.deposit(1000, new Date(2023, 1, 27));
+    expect(bankAccount.getStatement()).toBe("date\tcredit\tdebit\tbalance\n2/27/2023\t1000.00\t\t\t1000.00\n");
+  });
 
-})
+  it('should return a statement with a single withdrawal transaction', () => {
+    bankAccount.withdraw(500, new Date(2023, 1, 27));
+    expect(bankAccount.getStatement()).toBe("date\tcredit\tdebit\tbalance\n2/27/2023\t\t\t500.00\t-500.00\n");
+  });
+});
